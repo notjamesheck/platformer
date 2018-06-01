72be2e75488c21bb7066f147c34eaa92d1eac5d0
notjamesheck <224@holbertonschool.com>
2018-05-31 23:29:27 -0700

    update db fields for player, add 'new player' page

---
+++ priv/repo/migrations/20180531232658_add_fields_to_player_accounts.exs
@@ -0,0 +1,12 @@
+defmodule Platformer.Repo.Migrations.AddFieldsToPlayerAccounts do
+  use Ecto.Migration
+
+  def change do
+    alter table(:players) do
+      add :display_name, :string
+      add :password_digest, :string
+    end
+
+    create unique_index(:players, [:username])
+  end
+end
